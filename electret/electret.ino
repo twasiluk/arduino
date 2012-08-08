6e2aa89c177594434ba28083411a1bab4bd6fb4e
twasiluk <tomasz.wasiluk@polcode.com>
2012-08-08 15:18:03 +0200

    electret

---
+++ electret/electret.ino
@@ -0,0 +1,34 @@
+const int analogPin = 0;
+
+void setup() {
+  Serial.begin(9600); 
+}
+
+void loop() {
+  Serial.println(analogRead(A0));
+  delay(2);  
+}
+
+void minMax()
+{
+  
+  int mn = 1024;     // mn only decreases
+  int mx = 0;        // mx only increases
+
+  // Perform 10000 reads. Update mn and mx for each one.
+  for (int i = 0; i < 10000; ++i) {
+    int val = analogRead(analogPin);
+    mn = min(mn, val);
+    mx = max(mx, val);
+  }
+
+  // Send min, max and delta over Serial
+  Serial.print("m=");
+  Serial.print(mn);  
+  Serial.print(" M=");
+  Serial.print(mx);  
+  Serial.print(" D=");
+  Serial.print(mx-mn);  
+  Serial.println();  
+}
+
